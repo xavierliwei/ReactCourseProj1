eae194f3fab47c43669fb00381ca7a0d4a5e553e
Wei Li <wl2451@columbia.edu>
2020-05-09 23:51:33 -0700

    Updates

---
+++ precache-manifest.e72527635bbf3c4fd81f5711706a7e0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "614c0f8c3dd8ea2ead015049b9150dff",
+    "url": "/ReactCourseProj1/index.html"
+  },
+  {
+    "revision": "54a9ea64311bc3a6f26b",
+    "url": "/ReactCourseProj1/static/css/main.3897edea.chunk.css"
+  },
+  {
+    "revision": "ca96cb1d2607882b95b5",
+    "url": "/ReactCourseProj1/static/js/2.9a317882.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactCourseProj1/static/js/2.9a317882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54a9ea64311bc3a6f26b",
+    "url": "/ReactCourseProj1/static/js/main.bc0885fe.chunk.js"
+  },
+  {
+    "revision": "7477531982cb36558414",
+    "url": "/ReactCourseProj1/static/js/runtime-main.41c39422.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactCourseProj1/static/media/logo.5d5d9eef.svg"
+  }
+]);
